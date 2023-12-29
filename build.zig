9198284015063d334c40ac079df84fcbdf25b0b4
Igor Anić <igor.anic@gmail.com>
2023-12-29 14:12:30 +0100

    build benchmark binary

--- build.zig
+++ build.zig
@@ -82,10 +82,15 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    const bench = b.addExecutable(.{
+        .name = "benchmark",
+        .root_source_file = .{ .path = "src/benchmark.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(bench);
 }
